954fabe04281f3cd840695bba13688ee1fd50f72
Stefan Taubert <stefan.taubert@posteo.de>
2020-12-23 17:41:41 +0100

    from text_utils.spea

--- text_utils/__init__.py
+++ text_utils/__init__.py
@@ -1,3 +1,9 @@
+from text_utils.accents_dict import AccentsDict
 from text_utils.ipa2symb import IPAExtractionSettings
-from text_utils.text import (EngToIpaMode, Language, text_normalize,
-                             text_to_ipa, text_to_sentences, text_to_symbols)
+from text_utils.language import Language
+from text_utils.speakers_dict import SpeakersDict, SpeakersLogDict
+from text_utils.symbol_id_dict import SymbolIdDict
+from text_utils.symbols_map import SymbolsMap
+from text_utils.text import (EngToIpaMode, text_normalize, text_to_ipa,
+                             text_to_sentences, text_to_symbols)
+from text_utils.utils import deserialize_list, serialize_list
