ec5818840988297f4667c40e755dabb782017a1d
Jasmin Sternkopf <jasmin_sternkopf@web.de>
2021-01-04 23:21:33 +0100

    adds option to change arrow direction

--- text_utils/print_or_change_map_and_symbols.py
+++ text_utils/print_or_change_map_and_symbols.py
@@ -7,17 +7,28 @@ from text_utils.symbols_map import SymbolsMap
 def init_map_parser(parser: ArgumentParser):
   parser.add_argument("-p", "--path", type=str, required=True,
                       help="Path to .json-file containing the map")
+  parser.add_argument("-a", "--arrow_type", type=str, required=False,
+                      help="Sets the direction of the arrow")
   return print_map
 
 
-def print_map(path: str):
+def print_map(path: str, arrow_type: str):
   symbols_map = SymbolsMap.load(path)
+  arrow = get_correct_type_input(arrow_type)
   for map_output, map_input in symbols_map.items():
-    string_to_print = f"{map_input} \u2192 {map_output}"
+    string_to_print = f"{map_input} {arrow} {map_output}"
     if map_input == map_output:
       print(string_to_print)
     else:
       print('\033[1m' + string_to_print + '\033[0m')
+
+
+def get_correct_type_input(arrow_type: str) -> str:
+  while arrow_type not in ["weights", "interference"]:
+    arrow_type = input(
+      "Type must be either \"weights\" or \"interference\". Please define the type again: ")
+  arrow = "\u2190" if arrow_type == "interference" else "\u2192"
+  return arrow
 
 
 def init_symbol_parser(parser: ArgumentParser):
