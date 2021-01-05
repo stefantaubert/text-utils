b3626d73eb5bb2325678fd6a2c2a2b47dc563ab3
Jasmin Sternkopf <jasmin_sternkopf@web.de>
2021-01-05 21:23:09 +0100

    intermediate status

--- text_utils/print_or_change_map_and_symbols.py
+++ text_utils/print_or_change_map_and_symbols.py
@@ -1,5 +1,4 @@
 from argparse import ArgumentParser
-from typing import Mapping, Tuple
 
 from text_utils.symbols_map import SymbolsMap
 
@@ -22,6 +21,9 @@ def print_map(path: str, arrow_type: str):
     else:
       print('\033[1m' + string_to_print + '\033[0m')
 
+def print_bold(string_to_print: str, bold: bool):
+  if bold:
+    print()
 
 def space_or_nothing_as_word(symbol: str) -> str:
   if symbol == "":
@@ -56,11 +58,6 @@ def print_symbols(path: str):
         print_list.append(space_or_nothing_as_word(line))
   print(", ".join(print_list))
 
-def get_correct_input(upper_bound: int) -> int:
-  pos = int(input("Your input: ")) - 1
-  while pos < 0 or pos >= upper_bound:
-    pos = int(input(f"Please input a number between 1 and {upper_bound}: ")) - 1
-  return pos
 
 def init_change_parser(parser: ArgumentParser):
   parser.add_argument("-p", "--map_path", type=str, required=True,
@@ -91,8 +88,12 @@ def change_one_symbol_in_map(input_map: SymbolsMap, map_path: str, symbol_path: 
 def choose_key(input_map: SymbolsMap) -> str:
   print("The symbol corresponding to which key should be adjusted? Please input the corresponding number.")
   chosen_key = ""
-  for pos, (key, _) in enumerate(input_map.items()):
-    print(f"{pos+1}: {key}")
+  for pos, (key, value) in enumerate(input_map.items()):
+    string_to_print = f"{pos+1}: {key} (\u2190 {space_or_nothing_as_word(value)})"
+    if key == value:
+      print(string_to_print)
+    else:
+      print('\033[1m' + string_to_print + '\033[0m')
   chosen_key_pos = get_correct_input(len(input_map))
   for pos, (key, _) in enumerate(input_map.items()):
     if pos == chosen_key_pos:
@@ -106,16 +107,33 @@ def choose_symbol(symbol_path) -> str:
     lines = symbol_file.readlines()
   number_of_lines = open_file_and_print_symbols(lines)
   chosen_symbol_pos = get_correct_input(number_of_lines)
-  chosen_symbol = lines[chosen_symbol_pos].strip()[1:-1]
+  if chosen_symbol_pos < number_of_lines - 1:
+    chosen_symbol = lines[chosen_symbol_pos].strip()[1:-1]
+  else:
+    chosen_symbol = "NOTHING"
   return chosen_symbol
 
 
 def open_file_and_print_symbols(lines) -> int:
   number_of_lines = 0
+  contains_nothing = False
   for pos, line in enumerate(lines):
     line = line.strip()
     if len(line) > 0:
       line = line[1:-1]
-      print(f"{pos+1}: {line}")
+      if line == "":
+        contains_nothing = True
+      print(f"{pos+1}: {space_or_nothing_as_word(line)}")
       number_of_lines = pos + 1
+  if not contains_nothing:
+    number_of_lines += 1
+    print(f"{number_of_lines}: NOTHING")
   return number_of_lines
+
+
+def get_correct_input(upper_bound: int) -> int:
+  user_input = input("Your input: ")
+  while (not user_input.isnumeric()) or (int(user_input) < 1 or int(user_input) > upper_bound):
+    user_input = input(f"Please input a number between 1 and {upper_bound}: ")
+  pos = int(user_input) - 1
+  return pos
