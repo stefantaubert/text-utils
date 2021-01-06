581fc2098213019cd40d79f3681bbe284ba9ef64
Jasmin Sternkopf <jasmin_sternkopf@web.de>
2021-01-06 19:16:14 +0100

    small corrections

--- text_utils/print_or_change_map_and_symbols.py
+++ text_utils/print_or_change_map_and_symbols.py
@@ -22,10 +22,13 @@ def init_map_parser(parser: ArgumentParser) -> Callable[[str, str], None]:
 
 def print_map(path: str, arrow_type: str) -> None:
   symbols_map = SymbolsMap.load(path)
-  arrow = LEFT_ARROW if arrow_type == INFERENCE else RIGHT_ARROW
   for map_output, map_input in symbols_map.items():
-    string_to_print = f"{get_symbol_representation(map_input)} {arrow} {get_symbol_representation(map_output)}"
+    string_to_print = f"{get_symbol_representation(map_input)} {arrow(arrow_type)} {get_symbol_representation(map_output)}"
     print_bold_or_normal(string_to_print, map_input != map_output)
+
+
+def arrow(arrow_type: str) -> str:
+  return RIGHT_ARROW if arrow_type == WEIGHTS else LEFT_ARROW
 
 
 def print_bold_or_normal(string_to_print: str, bold: bool) -> None:
@@ -65,23 +68,26 @@ def print_symbols(path: str) -> None:
   print(", ".join(print_list))
 
 
-def init_change_parser(parser: ArgumentParser) -> Callable[[str, str, str, Optional[str], Optional[str]], None]:
+def init_change_parser(parser: ArgumentParser) -> Callable[[str, str, Optional[str], Optional[str], Optional[str]], None]:
   parser.add_argument("-p", "--map_path", type=str, required=True,
                       help="Path to .json-file containing the map")
   parser.add_argument("-s", "--symbol_path", type=str, required=True,
                       help="Path to file containing the allowed symbols")
-  parser.add_argument("-a", "--arrow_type", type=str, required=True,
-                      help="Sets the direction of the arrow", choices=["weights", "inference"])
-  parser.add_argument("--map_symbol", type=str, required=False,
+  parser.add_argument("-a", "--arrow_type", type=str, required=False,
+                      help="Sets the direction of the arrow", choices=ARROW_TYPES)
+  parser.add_argument("-m", "--map_symbol", type=str, required=False,
                       help="Key to which new symbol should be assigned")
-  parser.add_argument("--to", type=str, required=False,
+  parser.add_argument("-t", "--to", type=str, required=False,
                       help="Symbol that should be assigned to chosen key")
   return change_symbols_in_map
 
 
-def change_symbols_in_map(map_path: str, symbol_path: str, arrow_type: str, map_symbol: str = None, to: str = None) -> None:
+def change_symbols_in_map(map_path: str, symbol_path: str, arrow_type: Optional[str] = None, map_symbol: Optional[str] = None, to: Optional[str] = None) -> None:
   input_map = SymbolsMap.load(map_path)
   if map_symbol is None and to is None:
+    if arrow_type is None:
+      print("You have to specify the arrow type.")
+      return
     update = True
     while update:
       change_one_symbol_in_map(input_map, map_path, symbol_path, arrow_type)
@@ -133,11 +139,11 @@ def choose_key(input_map: SymbolsMap, arrow_type: str) -> str:
 
 
 def reverse_arrow(arrow_type: str) -> str:
-  return "\u2190" if arrow_type == "weights" else "\u2192"
+  return LEFT_ARROW if arrow_type == WEIGHTS else RIGHT_ARROW
 
 
 def choose_symbol(symbol_path: str) -> str:
-  print("Which symbol shoud be assigned to the chosen key? Please input the corresponding number.")
+  print("Which symbol should be assigned to the chosen key? Please input the corresponding number.")
   with open(symbol_path) as symbol_file:
     lines = symbol_file.readlines()
   number_of_lines = open_file_and_print_symbols(lines)
@@ -145,7 +151,7 @@ def choose_symbol(symbol_path: str) -> str:
   if chosen_symbol_pos < number_of_lines - 1:
     chosen_symbol = lines[chosen_symbol_pos].strip()[1:-1]
   else:
-    chosen_symbol = NOTHING
+    chosen_symbol = ""
   return chosen_symbol
 
 
