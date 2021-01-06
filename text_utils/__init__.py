12a2537b063a71980f863647b628f13c6a26e93f
Stefan Taubert <stefan.taubert@posteo.de>
2021-01-06 17:13:24 +0100

    added further functions to init

--- text_utils/__init__.py
+++ text_utils/__init__.py
@@ -5,7 +5,8 @@ from text_utils.language import Language
 from text_utils.speakers_dict import SpeakersDict, SpeakersLogDict
 from text_utils.symbol_id_dict import SymbolIdDict
 from text_utils.symbols_dict import SymbolsDict
-from text_utils.symbols_map import SymbolsMap
+from text_utils.symbols_map import (SymbolsMap, create_or_update_inference_map,
+                                    create_or_update_weights_map)
 from text_utils.text import (EngToIpaMode, text_normalize, text_to_ipa,
                              text_to_sentences, text_to_symbols)
 from text_utils.utils import deserialize_list, serialize_list
