5779cf161751ca055cac06ad582d34300c7b1c22
Jasmin Sternkopf <jasmin_sternkopf@web.de>
2021-01-06 16:08:26 +0100

    small improvements

--- text_utils/print_or_change_map_and_symbols.py
+++ text_utils/print_or_change_map_and_symbols.py
@@ -1,28 +1,41 @@
 from argparse import ArgumentParser
+from typing import Callable
 
 from text_utils.symbols_map import SymbolsMap
 
+NOTHING = "NOTHING"
+SPACE = "SPACE"
+WEIGHTS = "weights"
+INFERENCE = "inference"
+ARROW_TYPES = [WEIGHTS, INFERENCE]
+LEFT_ARROW = "\u2190"
+RIGHT_ARROW = "\u2192"
 
-def init_map_parser(parser: ArgumentParser):
+
+def init_map_parser(parser: ArgumentParser) -> Callable[[str, str], None]:
   parser.add_argument("-p", "--path", type=str, required=True,
                       help="Path to .json-file containing the map")
   parser.add_argument("-a", "--arrow_type", type=str, required=False,
-                      help="Sets the direction of the arrow")
+                      help="Sets the direction of the arrow", choices=ARROW_TYPES)
   return print_map
 
 
-def print_map(path: str, arrow_type: str):
+def print_map(path: str, arrow_type: str) -> None:
   symbols_map = SymbolsMap.load(path)
-  arrow = get_correct_type_input(arrow_type)
+  arrow = LEFT_ARROW if arrow_type == INFERENCE else RIGHT_ARROW
   for map_output, map_input in symbols_map.items():
-    string_to_print = f"{space_or_nothing_as_word(map_input)} {arrow} {space_or_nothing_as_word(map_output)}"
-    if map_input == map_output:
-      print(string_to_print)
-    else:
-      print('\033[1m' + string_to_print + '\033[0m')
+    string_to_print = f"{get_symbol_representation(map_input)} {arrow} {get_symbol_representation(map_output)}"
+    print_bold_if_true(string_to_print, map_input != map_output)
 
 
-def space_or_nothing_as_word(symbol: str) -> str:
+def print_bold_if_true(string_to_print: str, bold: bool):
+  if bold:
+    print('\033[1m' + string_to_print + '\033[0m')
+  else:
+    print(string_to_print)
+
+
+def get_symbol_representation(symbol: str) -> str:
   if symbol == "":
     return "NOTHING"
   if symbol == " ":
@@ -30,21 +43,13 @@ def space_or_nothing_as_word(symbol: str) -> str:
   return symbol
 
 
-def get_correct_type_input(arrow_type: str) -> str:
-  while arrow_type not in ["weights", "interference"]:
-    arrow_type = input(
-      "Type must be either \"weights\" or \"interference\". Please define the type again: ")
-  arrow = "\u2190" if arrow_type == "interference" else "\u2192"
-  return arrow
-
-
-def init_symbol_parser(parser: ArgumentParser):
+def init_symbol_parser(parser: ArgumentParser) -> Callable[[str], None]:
   parser.add_argument("-p", "--path", type=str, required=True,
                       help="Path to file containing the symbols")
   return print_symbols
 
 
-def print_symbols(path: str):
+def print_symbols(path: str) -> None:
   with open(path) as symbol_file:
     lines = symbol_file.readlines()
     print_list = []
@@ -52,5 +57,5 @@ def print_symbols(path: str):
       line = line.strip()
       if len(line) > 0:
         line = line[1:-1]
-        print_list.append(space_or_nothing_as_word(line))
+        print_list.append(get_symbol_representation(line))
   print(", ".join(print_list))
