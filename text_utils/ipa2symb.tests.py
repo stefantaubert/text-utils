32d554973bc93341e1786b67ad9cd4ca10b840b3
Jasmin Sternkopf <jasmin_sternkopf@web.de>
2021-02-02 21:46:38 +0100

    adds testcases (issue 11)

--- text_utils/ipa2symb.tests.py
+++ text_utils/ipa2symb.tests.py
@@ -3,8 +3,8 @@ from logging import getLogger
 
 from text_utils.ipa2symb import IPAExtractionSettings, extract_from_sentence
 from text_utils.language import Language
-from text_utils.text import (EngToIpaMode, text_normalize, text_to_ipa,
-                             text_to_symbols)
+from text_utils.text import (EngToIpaMode, check_is_ipa_and_return_closest_ipa,
+                             text_normalize, text_to_ipa, text_to_symbols)
 
 
 class UnitTests(unittest.TestCase):
@@ -19,6 +19,34 @@ class UnitTests(unittest.TestCase):
     res = extract_from_sentence(x, settings, getLogger())
 
     self.assertEqual(['_', '_', '_', '_', '_', '_', '_'], res)
+
+  # region check_is_ipa_and_return_closest_ipa
+
+  def test_check_is_ipa_and_return_closest_ipa__pure_ipa(self):
+    text = "ʊʌʒθ"
+    res = check_is_ipa_and_return_closest_ipa(text)
+
+    self.assertEqual(2, len(res))
+    self.assertTrue(res[0])
+    self.assertEqual(text, str(res[1]))
+
+  def test_check_is_ipa_and_return_closest_ipa__ipa_with_letters(self):
+    text = "Hʊʌʒ-θ2"
+    res = check_is_ipa_and_return_closest_ipa(text)
+
+    self.assertEqual(2, len(res))
+    self.assertFalse(res[0])
+    self.assertEqual("ʊʌʒθ", str(res[1]))
+
+  def test_check_is_ipa_and_return_closest_ipa__no_ipa(self):
+    text = "H-2"
+    res = check_is_ipa_and_return_closest_ipa(text)
+
+    self.assertEqual(2, len(res))
+    self.assertFalse(res[0])
+    self.assertEqual("", str(res[1]))
+
+  # endregion
 
     #y = u"ˈprɪnɪŋ, ɪn ðə ˈoʊnli sɛns wɪθ wɪʧ wi ər æt ˈprɛzənt kənˈsərnd, ˈdɪfərz frəm moʊst ɪf nɑt frəm ɔl ðə ɑrts ənd kræfts ˌrɛprɪˈzɛnɪd ɪn ðə ˌɛksəˈbɪʃən."
     #y = u"naw, æz ɔl bʊks nɑt pɹajmɛɹəli ɪntɛndəd æz pɪkt͡ʃɹ̩-bʊks kənsɪst pɹɪnsɪpli ʌv tajps kəmpowzd tə fɔɹm lɛtɹ̩pɹɛs"
