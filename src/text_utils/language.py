f7d244de13cf49fe76e3a3dea9299c75edbf8949
Stefan Taubert <stefan.taubert@posteo.de>
2021-06-23 11:05:32 +0200

    .

--- src/text_utils/language.py
+++ src/text_utils/language.py
@@ -6,3 +6,26 @@ class Language(IntEnum):
   ENG = 1
   CHN = 2
   GER = 3
+
+  def __repr__(self):
+    if self == self.IPA:
+      return str("ipa")
+    if self == self.ENG:
+      return str("eng")
+    if self == self.CHN:
+      return str("chn")
+    if self == self.GER:
+      return str("ger")
+    assert False
+
+
+lang_dict = {repr(x): x for x in list(Language)}
+
+
+def is_lang_from_str_supported(lang: str) -> bool:
+  return lang in lang_dict
+
+
+def get_lang_from_str(lang: str) -> Language:
+  assert is_lang_from_str_supported(lang)
+  return lang_dict[lang]
