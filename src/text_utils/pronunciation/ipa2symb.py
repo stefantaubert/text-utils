f34436a1f29fd4b4d26cbfe81f729adc1817e787
Jasmin Sternkopf <jasmin_sternkopf@web.de>
2021-09-14 22:40:55 +0200

    adds merge_fusion and tests (it works)

--- src/text_utils/pronunciation/ipa2symb.py
+++ src/text_utils/pronunciation/ipa2symb.py
@@ -61,42 +61,29 @@ def merge_fusion(symbols: Symbols, fusion_symbols: Set[Symbol]) -> Symbols:
   aux_symbols = list(symbols)
   fused_symbols = []
   while len(aux_symbols) != 0:
-    next_fused_symbols = get_next_fused_symbols(symbols, fusion_symbols)
+    next_fused_symbols, processed_index = get_next_fused_symbols_and_index(aux_symbols, fusion_symbols)
     fused_symbols.append(next_fused_symbols)
-    del aux_symbols[:len(next_fused_symbols)]
+    del aux_symbols[:processed_index + 1]
   return tuple(fused_symbols)
 
-def get_next_fused_symbols(symbols: Symbols, fusion_symbols: Set[Symbol]) -> Symbols:
+
+def get_next_fused_symbols_and_index(symbols: Symbols, fusion_symbols: Set[Symbol]) -> Tuple[Symbols, int]:
   if symbols[0] not in fusion_symbols:
-    return symbols[0]
-  
-  symbols_of_same_type_as_first_symbol = get_symbols_of_same_type_as_first_symbol(
-    symbols, fusion_symbols)
-  next_fused_symbols = get_next_symbols_of_same_type(symbols, symbols_of_same_type_as_first_symbol)
-  return tuple(next_fused_symbols)
-
-# def get_next_fused_symbols(symbols: Symbols, fusion_symbols: Set[Symbol]) -> Symbols:
-#   symbols_of_same_type_as_first_symbol = get_symbols_of_same_type_as_first_symbol(
-#     symbols, fusion_symbols)
-#   next_fused_symbols = get_next_symbols_of_same_type(symbols, symbols_of_same_type_as_first_symbol)
-#   return tuple(next_fused_symbols)
+    return symbols[0], 0
+  fused_fusion_symbols, processed_index = get_next_consecutive_fusion_symbols_and_index(symbols, fusion_symbols)
+  return fused_fusion_symbols, processed_index
 
 
-def get_next_symbols_of_same_type(symbols: Symbols, symbols_of_same_type: Set[Symbol]) -> Symbols:
-  consecutive_same_type_symbols = []
-  for symbol in symbols:
-    if symbol in symbols_of_same_type:
-      consecutive_same_type_symbols.append(symbol)
+def get_next_consecutive_fusion_symbols_and_index(symbols: Symbols, fusion_symbols: Set[Symbol]) -> Tuple[Symbols, int]:
+  consecutive_fusion_symbols = symbols[0]
+  processed_index = 0
+  for symbol in symbols[1:]:
+    if symbol in fusion_symbols:
+      consecutive_fusion_symbols += symbol
+      processed_index += 1
     else:
       break
-  return consecutive_same_type_symbols
-
-
-def get_symbols_of_same_type_as_first_symbol(symbols: Symbols, fusion_symbols: Set[Symbol]) -> Symbols:
-  if symbols[0] in fusion_symbols:
-    return fusion_symbols
-  not_fusion_symbols_in_symbols = set(symbols) - fusion_symbols
-  return not_fusion_symbols_in_symbols
+  return consecutive_fusion_symbols, processed_index
 
 
 def merge_together(symbols: Symbols, merge_symbols: Set[Symbol], ignore_merge_symbols: Set[Symbol]) -> Symbols:
