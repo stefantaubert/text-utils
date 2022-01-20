f54aad0deff7b518a117c19c655b3de154bc5931
Stefan Taubert <stefan.taubert@posteo.de>
2022-01-20 17:48:09 +0100

    .

--- src/text_utils/string_format.py
+++ src/text_utils/string_format.py
@@ -7,6 +7,14 @@ String = str
 TextString = String
 SymbolsString = String
 
+SPACED_WORD_SEP = "  "
+SPACED_SYMBOL_SEP = " "
+
+TEXT_WORD_SEP = " "
+TEXT_SYMBOL_SEP = ""
+
+TUPLE_WORD_SEP = " "
+
 
 class StringFormat(IntEnum):
   TEXT = 0
@@ -18,23 +26,28 @@ class StringFormat(IntEnum):
   def convert_symbols_to_string(self, symbols: Symbols) -> String:
     return convert_symbols_to_string(symbols, self)
 
-  # def __str__(self) -> str:
-  #   if self == self.TEXT:
-  #     return "TEXT"
+  def can_convert_string_to_symbols(self, string: String) -> bool:
+    return can_convert_string_to_symbols(string, self)
 
-  #   if self == self.SYMBOLS:
-  #     return "SYMBOLS"
+  def can_convert_symbols_to_string(self, symbols: Symbols) -> bool:
+    return can_convert_symbols_to_string(symbols, self)
 
-  #   assert False
-    
-  def __repr__(self) -> str:
-    if self == self.TEXT:
-      return "Text"
 
-    if self == self.SYMBOLS:
-      return "Symbols"
+def can_convert_string_to_symbols(string: String, string_format: StringFormat) -> bool:
+  if string_format == StringFormat.SYMBOLS:
+    return can_convert_symbols_string_to_symbols(string)
+  if string_format == StringFormat.TEXT:
+    return True
+  assert False
 
-    assert False
+
+def can_convert_symbols_to_string(symbols: Symbols, string_format: StringFormat) -> bool:
+  if string_format == StringFormat.SYMBOLS:
+    return can_convert_symbols_to_symbols_string(symbols)
+  if string_format == StringFormat.TEXT:
+    return True
+  assert False
+
 
 def get_other_format(string_format: StringFormat) -> StringFormat:
   if string_format == StringFormat.SYMBOLS:
@@ -42,6 +55,7 @@ def get_other_format(string_format: StringFormat) -> StringFormat:
   if string_format == StringFormat.TEXT:
     return StringFormat.SYMBOLS
   assert False
+
 
 def convert_string_to_symbols(string: String, string_format: StringFormat) -> Symbols:
   if string_format == StringFormat.SYMBOLS:
@@ -60,18 +74,18 @@ def convert_symbols_to_string(symbols: Symbols, string_format: StringFormat) -> 
 
 
 def can_convert_symbols_string_to_symbols(symbols_string: String) -> bool:
-  words = symbols_string.split("  ")
+  words = symbols_string.split(SPACED_WORD_SEP)
   for word in words:
-    if word.startswith(" ") or word.endswith(" "):
+    if word.startswith(SPACED_SYMBOL_SEP) or word.endswith(SPACED_SYMBOL_SEP):
       return False
   return True
 
 
 def convert_symbols_string_to_symbols(symbols_string: SymbolsString) -> Symbols:
   assert can_convert_symbols_string_to_symbols(symbols_string)
-  words = symbols_string.split("  ")
-  words_symbols = [tuple(word.split(" ")) for word in words]
-  result = symbols_join(words_symbols, join_symbol=" ")
+  words = symbols_string.split(SPACED_WORD_SEP)
+  words_symbols = [tuple(word.split(SPACED_SYMBOL_SEP)) for word in words]
+  result = symbols_join(words_symbols, join_symbol=TUPLE_WORD_SEP)
   result = symbols_ignore(result, ignore={""})
   return result
 
@@ -79,10 +93,10 @@ def convert_symbols_string_to_symbols(symbols_string: SymbolsString) -> Symbols:
 def can_convert_symbols_to_symbols_string(symbols: Symbols) -> bool:
   if "" in symbols:
     return False
-  words = symbols_split(symbols, split_symbols=" ")
+  words = symbols_split(symbols, split_symbols=TUPLE_WORD_SEP)
   symbols = (symbol for word in words for symbol in word)
   for symbol in symbols:
-    symbol_is_no_space_but_contains_space = symbol != " " and " " in symbol
+    symbol_is_no_space_but_contains_space = symbol != SPACED_SYMBOL_SEP and SPACED_SYMBOL_SEP in symbol
     if symbol_is_no_space_but_contains_space:
       return False
   return True
@@ -90,9 +104,9 @@ def can_convert_symbols_to_symbols_string(symbols: Symbols) -> bool:
 
 def convert_symbols_to_symbols_string(symbols: Symbols) -> SymbolsString:
   assert can_convert_symbols_to_symbols_string(symbols)
-  words = symbols_split(symbols, split_symbols=" ")
-  words_symbols_str = (' '.join(word) for word in words)
-  symbols_str = "  ".join(words_symbols_str)
+  words = symbols_split(symbols, split_symbols=TUPLE_WORD_SEP)
+  words_symbols_str = (SPACED_SYMBOL_SEP.join(word_symbols) for word_symbols in words)
+  symbols_str = SPACED_WORD_SEP.join(words_symbols_str)
   return symbols_str
 
 
@@ -102,5 +116,5 @@ def convert_text_string_to_symbols(text_string: TextString) -> Symbols:
 
 
 def convert_symbols_to_text_string(symbols: Symbols) -> TextString:
-  result = ''.join(symbols)
+  result = TEXT_SYMBOL_SEP.join(symbols)
   return result
