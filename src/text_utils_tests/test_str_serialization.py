dacb40cc9e5bb6a4c09513cfd5b2b6824a356b3b
Jasmin Sternkopf <jasmin_sternkopf@web.de>
2022-01-28 15:30:47 +0100

    intermediate status with old versions

---
+++ src/text_utils_tests/test_str_serialization.py
@@ -0,0 +1,22 @@
+from text_utils.str_serialization import str_deserialization
+
+
+def test_str_serialization():
+  text = "St 1s   3s"
+  res = list(str_deserialization(text, " "))
+
+  assert res == ["St", "1s", " ", "3s"]
+
+
+def test_str_serialization_five_spaces():
+  text = "St     5s"
+  res = list(str_deserialization(text, " "))
+
+  assert res == ["St", " ", " ", "5s"]
+
+
+def test_str_serialization_seven_spaces():
+  text = "St       7s"
+  res = list(str_deserialization(text, " "))
+
+  assert res == ["St", " ", " ", " ", "7s"]
