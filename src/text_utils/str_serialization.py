38e104e7109dcca55221c868ead6831b775719fd
Jasmin Sternkopf <jasmin_sternkopf@web.de>
2022-01-28 16:49:03 +0100

    intermediate status

--- src/text_utils/str_serialization.py
+++ src/text_utils/str_serialization.py
@@ -1,29 +1,87 @@
-from typing import Generator
+from typing import Generator, Iterable
 
 from text_utils.types import Symbol
 
 
-def can_serialize(text: str, split_symbol: Symbol) -> bool:
+def can_serialize(symbols: Iterable[str], split_symbol: Symbol) -> bool:
+  pass
+
+
+def str_serialization(symbols: Iterable[str], split_symbol: Symbol) -> str:
+  assert len(split_symbol) == 1
+  text = split_symbol.join(symbols)
+  return text
+
+
+def can_deserialize(text: str, split_symbol: Symbol) -> bool:
   if split_symbol * 2 in text:
     if not split_symbol * 3 in text:
       pass
 
 
 def str_deserialization(text: str, split_symbol: Symbol) -> Generator[Symbol, None, None]:
-  #assert can_serialize(text, split_symbol)
+  #assert can_deserialize(text, split_symbol)
+  assert len(split_symbol) == 1
   symbol = ""
-  split_symbol_before = False
+  yield_subsequent_split_symbol = None
   for char in text:
-    if char != split_symbol:
-      symbol += char
-    elif symbol != "":
-      yield symbol
-      symbol = ""
-      split_symbol_before = True
-    elif split_symbol_before:
-      yield split_symbol
-      split_symbol_before = False
+    if char == split_symbol:
+      if symbol == "":
+        if yield_subsequent_split_symbol is None:
+          yield char
+          yield_subsequent_split_symbol = False
+        if yield_subsequent_split_symbol:
+          yield char
+          yield_subsequent_split_symbol = False
+        else:
+          yield_subsequent_split_symbol = True
+      else:
+        yield symbol
+        symbol = ""
+        yield_subsequent_split_symbol = True
     else:
-      split_symbol_before = True
+      symbol += char
   if symbol != "":
     yield symbol
+
+# def str_deserialization(text: str, split_symbol: Symbol) -> Generator[Symbol, None, None]:
+#   #assert can_deserialize(text, split_symbol)
+#   assert len(split_symbol) == 1
+#   symbol = ""
+#   split_symbol_before = False
+#   for char in text:
+#     if char != split_symbol:
+#       symbol += char
+#     else:
+#       if symbol != "":
+#         yield symbol
+#         symbol = ""
+#         split_symbol_before = True
+#       else:
+#         if split_symbol_before:
+#           yield split_symbol
+#           split_symbol_before = False
+#         else:
+#           split_symbol_before = True
+#   if symbol != "":
+#     yield symbol
+
+# def str_deserialization(text: str, split_symbol: Symbol) -> Generator[Symbol, None, None]:
+#   #assert can_deserialize(text, split_symbol)
+#   assert len(split_symbol) == 1
+#   symbol = ""
+#   split_symbol_before = False
+#   for char in text:
+#     if char != split_symbol:
+#       symbol += char
+#     elif symbol != "":
+#       yield symbol
+#       symbol = ""
+#       split_symbol_before = True
+#     elif split_symbol_before:
+#       yield split_symbol
+#       split_symbol_before = False
+#     else:
+#       split_symbol_before = True
+#   if symbol != "":
+#     yield symbol
--- src/text_utils_tests/test_str_serialization.py
+++ src/text_utils_tests/test_str_serialization.py
@@ -1,29 +1,135 @@
-from text_utils.str_serialization import str_deserialization
+from text_utils.str_serialization import str_deserialization, str_serialization
+
+# region str_deserialization
 
 
-def test_str_serialization():
-  text = "St 1s   3s"
+def test_str_serialization__single_non_split_symbol():
+  symbols = ("a",)
+  res = str_serialization(symbols, " ")
+
+  assert res == "a"
+
+
+def test_str_serialization__two_symbols_no_split_symbol():
+  symbols = ("a", "a")
+  res = str_serialization(symbols, " ")
+
+  assert res == "a a"
+
+
+def test_str_serialization__symbol_split_symbol_symbol():
+  symbols = ("a", " ", "a")
+  res = str_serialization(symbols, " ")
+
+  assert res == "a   a"
+
+
+def test_str_serialization__one_symbol_consists_of_two_chars():
+  symbols = ("ab", "a")
+  res = str_serialization(symbols, " ")
+
+  assert res == "ab a"
+
+
+def test_str_serialization__split_symbol_at_end():
+  symbols = ("a", "a", " ")
+  res = str_serialization(symbols, " ")
+
+  assert res == "a a  "
+
+
+def test_str_serialization__split_symbol_at_beginning():
+  symbols = (" ", "a", "a")
+  res = str_serialization(symbols, " ")
+
+  assert res == "  a a"
+
+
+def test_str_serialization__only_one_split_symbol():
+  symbols = (" ",)
+  res = str_serialization(symbols, " ")
+
+  assert res == " "
+
+
+def test_str_serialization__only_two_split_symbols():
+  symbols = (" ", " ")
+  res = str_serialization(symbols, " ")
+
+  assert res == "   "
+
+
+def test_str_serialization__only_three_split_symbols():
+  symbols = (" ", " ", " ")
+  res = str_serialization(symbols, " ")
+
+  assert res == "     "
+
+
+def test_str_serialization__symbols_is_empty():
+  symbols = ()
+  res = str_serialization(symbols, " ")
+
+  assert res == ""
+
+# endregion
+
+# region str_deserialization
+
+
+def test_str_deserialization__component_test():
+  text = "  a ab   a  "
   res = list(str_deserialization(text, " "))
 
-  assert res == ["St", "1s", " ", "3s"]
+  assert res == [" ", "a", "ab", " ", "a", " "]
 
 
-def test_str_serialization_five_spaces():
-  text = "St     5s"
+def test_str_deserialization_three_spaces():
+  text = "a     a"
   res = list(str_deserialization(text, " "))
 
-  assert res == ["St", " ", " ", "5s"]
+  assert res == ["a", " ", " ", "a"]
 
 
-def test_str_serialization_seven_spaces():
-  text = "St       7s"
+def test_str_deserialization_five_spaces():
+  text = "a     a"
   res = list(str_deserialization(text, " "))
 
-  assert res == ["St", " ", " ", " ", "7s"]
+  assert res == ["a", " ", " ", "a"]
 
 
-def test_str_serialization_two_spaces_at_end():
-  text = "St 1s  "
+def test_str_deserialization_seven_spaces():
+  text = "a       a"
   res = list(str_deserialization(text, " "))
 
-  assert res == ["St", "1s", " "]
+  assert res == ["a", " ", " ", " ", "a"]
+
+
+def test_str_deserialization_two_spaces_at_end():
+  text = "a  "
+  res = list(str_deserialization(text, " "))
+
+  assert res == ["a", " "]
+
+
+def test_str_deserialization_two_spaces_at_beginning():
+  text = "  a"
+  res = list(str_deserialization(text, " "))
+
+  assert res == [" ", "a"]
+
+
+def test_str_deserialization_only_spaces():
+  text = "   "
+  res = list(str_deserialization(text, " "))
+
+  assert res == [" ", " "]
+
+
+def test_str_deserialization_empty_text():
+  text = ""
+  res = list(str_deserialization(text, " "))
+
+  assert res == []
+
+# endregion
